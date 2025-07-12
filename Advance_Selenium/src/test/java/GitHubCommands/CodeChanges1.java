44d9d1568b5da2223d2d6a63c7dec702394ce56c
anjalikumari18 <anjalikumarijsr1811@gmail.com>
2025-07-12 11:37:46 +0530

    2nd commit>>Made changes in test java

---
+++ Advance_Selenium/src/test/java/GitHubCommands/CodeChanges1.java
@@ -0,0 +1,13 @@
+package GitHubCommands;
+
+public class CodeChanges1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Testing the push commands");
+
+	}
+
+}
