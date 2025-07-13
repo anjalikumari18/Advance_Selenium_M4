671b6e265197c175950bb6f29ad98923723e7d4f
anjalikumari18 <anjalikumarijsr1811@gmail.com>
2025-07-13 09:06:48 +0530

    Git conflict instigation

---
+++ Advance_Selenium/src/test/java/GitHubCommands/GitConflict.java
@@ -0,0 +1,13 @@
+package GitHubCommands;
+
+public class GitConflict {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("I am trying to make s git conflict");
+
+	}
+
+}
